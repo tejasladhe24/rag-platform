ae78044032f85e40f8462f759b569139c7165a38
Tejas Ladhe <tejasladhe24@gmail.com>
2025-03-17 19:32:44 +0530

    Update auth.tsx

--- components/form/auth.tsx
+++ components/form/auth.tsx
@@ -5,8 +5,9 @@ import register from "@/actions/auth/register";
 import { useAction } from "@/hooks/use-action";
 import { AuthInputType, authSchema } from "@/schema";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Loader2 } from "lucide-react";
+import { Eye, EyeOff, Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { Button } from "../ui/button";
@@ -18,6 +19,7 @@ export const AuthForm = ({ action }: { action: "login" | "register" }) => {
     resolver: zodResolver(authSchema),
     defaultValues: { email: "", password: "" },
   });
+  const [showPassword, setShowPassword] = useState(false);
 
   const router = useRouter();
 
@@ -42,48 +44,6 @@ export const AuthForm = ({ action }: { action: "login" | "register" }) => {
     if (action === "login") await executeLogin(values);
     else await executeRegister(values);
   };
-
-  if (action === "login")
-    return (
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit(onSubmit)}
-          className="flex flex-col gap-4"
-        >
-          <div className="flex flex-col gap-2">
-            <FormField
-              name="email"
-              control={form.control}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="email">Email</FormLabel>
-                  <FormControl>
-                    <Input placeholder="acme@example.com" {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-            <FormField
-              name="password"
-              control={form.control}
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel htmlFor="password">Password</FormLabel>
-                  <FormControl>
-                    <Input placeholder="********" {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-          </div>
-
-          <Button type="submit" disabled={isLoading}>
-            Login
-            {isLoading && <Loader2 className="animate-spin" />}
-          </Button>
-        </form>
-      </Form>
-    );
 
   return (
     <Form {...form}>
@@ -111,14 +71,42 @@ export const AuthForm = ({ action }: { action: "login" | "register" }) => {
               <FormItem>
                 <FormLabel htmlFor="password">Password</FormLabel>
                 <FormControl>
-                  <Input placeholder="********" {...field} />
+                  <Input
+                    type={showPassword ? "text" : "password"}
+                    placeholder="********"
+                    {...field}
+                  />
                 </FormControl>
+                <div className="flex items-center justify-end">
+                  <Button
+                    type="button"
+                    className="p-0"
+                    variant={"link"}
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword && (
+                      <div className="flex items-center gap-2">
+                        <EyeOff className="text-muted-foreground" />
+                        <span className="text-muted-foreground">
+                          Show Password
+                        </span>
+                      </div>
+                    )}
+                    {!showPassword && (
+                      <div className="flex items-center gap-2">
+                        <Eye />
+                        <span>Hide Password</span>
+                      </div>
+                    )}
+                  </Button>
+                </div>
               </FormItem>
             )}
           />
         </div>
-        <Button type="submit" disabled={isLoading}>
-          Register
+
+        <Button type="submit" disabled={isLoading} className="capitalize">
+          {action}
           {isLoading && <Loader2 className="animate-spin" />}
         </Button>
       </form>
